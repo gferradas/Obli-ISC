a288c015fd1c1e4307c36e1427ec48baf09afde6
gferradas <guillermoferr1507@gmail.com>
2023-06-16 17:03:59 -0300

    se agregan comentarios para las funciones

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#seccion de comprobacion si tiene aws cli en el cliente sino se instala
 if command -v aws &> /dev/null
 then
     echo "cumple requerimientos"
@@ -13,11 +15,12 @@ fi
 echo "#####################################################################################"
 
 echo "Actualice las credenciales de aws"
+
 sleep 1
-vim $HOME/.aws/credentials
-
-
+#se abri vi para que se modifquen las credenciales de la cli
+vi $HOME/.aws/credentials
+#se exporta el arn del rol LabRole para usarlo como variable de entorno para la variable de terraform
 export TF_VAR_arn_role=$(aws iam get-role --role-name LabRole --query 'Role.Arn' --output text)
-
+#Se busca en la carpeta src y se itera los archivos en busca de los docker file donde se construyen y pushean
 find $HOME/Obli-ISC/src -type d -exec sh -c 'cd "{}" && docker build -t "gferradas/$(basename {}):v2" . && docker push "gferradas/$(basename {}):v2"' \;
 
